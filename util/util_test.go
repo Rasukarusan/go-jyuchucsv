85fa041d40d489d7c8eefd4f1c6803f59e64765d
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-08-03 10:27:30 +0900

    ユニットテストのactual,expectedをgot,wantに変更
    refs #master

--- util/util_test.go
+++ util/util_test.go
@@ -9,32 +9,32 @@ import (
 
 func TestGetDate(t *testing.T) {
 	time, _ := time.Parse("2006-01-02 15:04", "2014-12-31 18:10")
-	actual := Date(time)
-	expected := "2014/12/31 18:10"
-	if actual != expected {
-		t.Errorf(result(actual, expected))
+	got := Date(time)
+	want := "2014/12/31 18:10"
+	if got != want {
+		t.Errorf(result(got, want))
 	}
 }
 
 func TestGetRandomStr(t *testing.T) {
 	time, _ := time.Parse("2006-01-02 15:04", "2014-12-31 18:10")
-	actual := UnixNanoStr(time)
-	expected := "1420049400000000000"
-	if actual != expected {
-		t.Errorf(result(actual, expected))
+	got := UnixNanoStr(time)
+	want := "1420049400000000000"
+	if got != want {
+		t.Errorf(result(got, want))
 	}
 }
 
 func TestReadStdin(t *testing.T) {
-	expected := "100"
+	want := "100"
 	var stdin bytes.Buffer
-	stdin.Write([]byte(expected + "\n"))
-	actual := ReadStdin(&stdin)
-	if actual != expected {
-		t.Errorf(result(actual, expected))
+	stdin.Write([]byte(want + "\n"))
+	got := ReadStdin(&stdin)
+	if got != want {
+		t.Errorf(result(got, want))
 	}
 }
 
-func result(actual interface{}, expected interface{}) string {
-	return fmt.Sprintf("\nactual: (%T) %#v\nexpected: (%T) %#v", actual, actual, expected, expected)
+func result(got interface{}, want interface{}) string {
+	return fmt.Sprintf("\ngot: (%T) %#v\nwant: (%T) %#v", got, got, want, want)
 }
